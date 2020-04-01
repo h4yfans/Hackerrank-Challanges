488d86c91166149e89bc9acaf9bd43d4be98e9d6
h4yfans <kaan94karaca@gmail.com>
2020-04-01 18:32:56 +0300

    new year chaos

--- 002. Arrays/new_year_chaos.go
+++ 002. Arrays/new_year_chaos.go
@@ -4,24 +4,48 @@ import "fmt"
 
 // Complete the minimumBribes function below.
 func minimumBribes(q []int32) {
-	var bribe int32
+	lengthQueue := len(q)
 
-	arrlen := int32(len(q))
+	var sortedSlice = make([]int32, lengthQueue)
+	count_total := 0
+	var a int32 = 0
 
-	for i := arrlen - 1; i >= 0; i-- {
-		if q[i]-i+1 > 2 {
-			fmt.Println("Too chaotic")
-			return
+	for i := 0; i < lengthQueue; i++ {
+		sortedSlice[i] = int32(i) + 1
+	}
+
+	for i := 0; i < lengthQueue; i++ {
+
+		// Find the right a position
+		if i+2 < lengthQueue {
+			if sortedSlice[i+2] == q[i] {
+				a = int32(i + 2)
+			} else if sortedSlice[i+1] == q[i] {
+				a = int32(i + 1)
+			} else if sortedSlice[i] == q[i] {
+				continue
+			} else {
+				fmt.Println("Too chaotic")
+				return
+			}
+		} else if i+1 < lengthQueue {
+			if sortedSlice[i+1] == q[i] {
+				a = int32(i + 1)
+			} else if sortedSlice[i] == q[i] {
+				continue
+			}
 		}
 
-		for j := i - 1; i > 0 && j >= 0; j-- {
-			if q[j] > q[i] {
-				bribe++
-			}
+		for q[i] != sortedSlice[i] {
+			sortedSlice[a], sortedSlice[a-1] = sortedSlice[a-1], sortedSlice[a]
+			count_total += 1
+			a -= 1
 		}
 	}
 
-	fmt.Println(bribe)
+	fmt.Println(count_total)
+
+	return
 }
 
 func main() {
